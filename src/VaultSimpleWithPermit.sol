ee9dc303eba7ee0893a4f1dab8de4340b32919a0
saucepoint <saucepoint@protonmail.com>
2024-01-17 17:11:40 -0500

    passing test

---
+++ src/VaultSimpleWithPermit.sol
@@ -0,0 +1,83 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import "evc-playground/vaults/VaultSimple.sol";
+import "permit2/src/interfaces/IPermit2.sol";
+
+/// @title VaultSimpleWithPermit
+/// @notice This contract extends VaultSimple to support Permit2 deposits
+contract VaultSimpleWithPermit is VaultSimple {
+    using SafeTransferLib for ERC20;
+    using FixedPointMathLib for uint256;
+
+    IPermit2 public immutable permit2;
+
+    constructor(IEVC _evc, ERC20 _asset, string memory _name, string memory _symbol, IPermit2 _permit2)
+        VaultSimple(_evc, _asset, _name, _symbol)
+    {
+        permit2 = _permit2;
+    }
+
+    function _permitTransferFrom(
+        address msgSender,
+        uint256 amount,
+        ISignatureTransfer.PermitTransferFrom calldata permitData,
+        bytes calldata signature
+    ) internal {
+        require(permitData.permitted.token == address(asset), "INVALID_TOKEN");
+        ISignatureTransfer.SignatureTransferDetails memory transferDetails =
+            ISignatureTransfer.SignatureTransferDetails({to: address(this), requestedAmount: amount});
+        permit2.permitTransferFrom(permitData, transferDetails, msgSender, signature);
+    }
+
+    /// @dev Deposits a certain amount of assets for a receiver.
+    /// @param assets The assets to deposit.
+    /// @param receiver The receiver of the deposit.
+    /// @return shares The shares equivalent to the deposited assets.
+    function depositWithPermit(
+        uint256 assets,
+        address receiver,
+        ISignatureTransfer.PermitTransferFrom calldata permitData,
+        bytes calldata signature
+    ) public callThroughEVC nonReentrant returns (uint256 shares) {
+        address msgSender = _msgSender();
+
+        takeVaultSnapshot();
+
+        // Check for rounding error since we round down in previewDeposit.
+        require((shares = previewDeposit(assets)) != 0, "ZERO_SHARES");
+
+        // transferFrom with Permit2 instead of ERC20.safeTransferFrom
+        _permitTransferFrom(msgSender, assets, permitData, signature);
+
+        _mint(receiver, shares);
+
+        emit Deposit(msgSender, receiver, assets, shares);
+
+        requireAccountAndVaultStatusCheck(address(0));
+    }
+
+    /// @dev Mints a certain amount of shares for a receiver.
+    /// @param shares The shares to mint.
+    /// @param receiver The receiver of the mint.
+    /// @return assets The assets equivalent to the minted shares.
+    // function mintWithPermit(
+    //     uint256 shares,
+    //     address receiver
+    // ) public virtual override callThroughEVC nonReentrant returns (uint256 assets) {
+    //     address msgSender = _msgSender();
+
+    //     takeVaultSnapshot();
+
+    //     assets = previewMint(shares); // No need to check for rounding error, previewMint rounds up.
+
+    //     // Need to transfer before minting or ERC777s could reenter.
+    //     asset.safeTransferFrom(msgSender, address(this), assets);
+
+    //     _mint(receiver, shares);
+
+    //     emit Deposit(msgSender, receiver, assets, shares);
+
+    //     requireAccountAndVaultStatusCheck(address(0));
+    // }
+}
---
+++ test/VaultSimpleWithPermit.t.sol
@@ -0,0 +1,145 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+pragma solidity ^0.8.19;
+
+import "forge-std/Test.sol";
+import "solmate/src/test/utils/mocks/MockERC20.sol";
+import "evc/EthereumVaultConnector.sol";
+
+import {DeployPermit2} from "permit2/test/utils/DeployPermit2.sol";
+import {PermitSignature} from "permit2/test/utils/PermitSignature.sol";
+
+import {VaultSimpleWithPermit} from "../src/VaultSimpleWithPermit.sol";
+import "permit2/src/interfaces/IPermit2.sol";
+
+contract VaultSimpleWithPermitTest is Test, DeployPermit2, PermitSignature {
+    IEVC evc;
+    MockERC20 underlying;
+    VaultSimpleWithPermit vault;
+    IPermit2 permit2;
+
+    address alice;
+    uint256 alicePK;
+
+    error NotAuthorized();
+    error ControllerDisabled();
+
+    function setUp() public {
+        deployPermit2();
+        permit2 = IPermit2(PERMIT2_ADDRESS);
+        evc = new EthereumVaultConnector();
+        underlying = new MockERC20("Mock Token", "TKN", 18);
+        vault = new VaultSimpleWithPermit(evc, underlying, "Mock Token Vault", "vTKN", permit2);
+
+        (alice, alicePK) = makeAddrAndKey("alice");
+
+        // alice max-approves Permit2
+        vm.prank(alice);
+        underlying.approve(address(permit2), type(uint256).max);
+    }
+
+    function testSingleDepositWithPermitWithdraw() public {
+        uint256 amount = 1e18;
+
+        uint256 aliceUnderlyingAmount = amount;
+        underlying.mint(alice, aliceUnderlyingAmount);
+
+        // Alice signs permit to spend 1e18 tokens
+        uint256 nonce = 0;
+        ISignatureTransfer.PermitTransferFrom memory permitData = defaultERC20PermitTransfer(address(underlying), nonce);
+        bytes memory sig = getPermitTransferSignature2(permitData, alicePK, permit2.DOMAIN_SEPARATOR());
+
+        uint256 alicePreDepositBal = underlying.balanceOf(alice);
+
+        vm.prank(alice);
+        uint256 aliceShareAmount = vault.depositWithPermit(aliceUnderlyingAmount, alice, permitData, sig);
+
+        // Expect exchange rate to be 1:1 on initial deposit.
+        assertEq(aliceUnderlyingAmount, aliceShareAmount);
+        assertEq(vault.previewWithdraw(aliceShareAmount), aliceUnderlyingAmount);
+        assertEq(vault.previewDeposit(aliceUnderlyingAmount), aliceShareAmount);
+        assertEq(vault.totalSupply(), aliceShareAmount);
+        assertEq(vault.totalAssets(), aliceUnderlyingAmount);
+        assertEq(vault.balanceOf(alice), aliceShareAmount);
+        assertEq(vault.convertToAssets(vault.balanceOf(alice)), aliceUnderlyingAmount);
+        assertEq(underlying.balanceOf(alice), alicePreDepositBal - aliceUnderlyingAmount);
+
+        vm.prank(alice);
+        vault.withdraw(aliceUnderlyingAmount, alice, alice);
+
+        assertEq(vault.totalAssets(), 0);
+        assertEq(vault.balanceOf(alice), 0);
+        assertEq(vault.convertToAssets(vault.balanceOf(alice)), 0);
+        assertEq(underlying.balanceOf(alice), alicePreDepositBal);
+    }
+
+    function testSingleMintRedeem(uint128 amount, uint256 seed) public {
+        if (amount == 0) amount = 1;
+
+        uint256 aliceShareAmount = amount;
+
+        address alice = address(0xABCD);
+
+        underlying.mint(alice, aliceShareAmount);
+
+        vm.prank(alice);
+        underlying.approve(address(vault), aliceShareAmount);
+        assertEq(underlying.allowance(alice, address(vault)), aliceShareAmount);
+
+        uint256 alicePreDepositBal = underlying.balanceOf(alice);
+
+        vm.prank(alice);
+        uint256 aliceUnderlyingAmount;
+        if (seed % 2 == 0) {
+            aliceUnderlyingAmount = vault.mint(aliceShareAmount, alice);
+        } else {
+            evc.call(address(vault), alice, 0, abi.encodeWithSelector(vault.mint.selector, aliceShareAmount, alice));
+            aliceUnderlyingAmount = vault.convertToAssets(aliceShareAmount);
+        }
+
+        // Expect exchange rate to be 1:1 on initial mint.
+        assertEq(aliceShareAmount, aliceUnderlyingAmount);
+        assertEq(vault.previewWithdraw(aliceShareAmount), aliceUnderlyingAmount);
+        assertEq(vault.previewDeposit(aliceUnderlyingAmount), aliceShareAmount);
+        assertEq(vault.totalSupply(), aliceShareAmount);
+        assertEq(vault.totalAssets(), aliceUnderlyingAmount);
+        assertEq(vault.balanceOf(alice), aliceUnderlyingAmount);
+        assertEq(vault.convertToAssets(vault.balanceOf(alice)), aliceUnderlyingAmount);
+        assertEq(underlying.balanceOf(alice), alicePreDepositBal - aliceUnderlyingAmount);
+
+        vm.prank(alice);
+        if (seed % 2 == 0) {
+            vault.redeem(aliceShareAmount, alice, alice);
+        } else {
+            evc.call(
+                address(vault), alice, 0, abi.encodeWithSelector(vault.redeem.selector, aliceShareAmount, alice, alice)
+            );
+        }
+
+        assertEq(vault.totalAssets(), 0);
+        assertEq(vault.balanceOf(alice), 0);
+        assertEq(vault.convertToAssets(vault.balanceOf(alice)), 0);
+        assertEq(underlying.balanceOf(alice), alicePreDepositBal);
+    }
+
+    function getPermitTransferSignature2(
+        ISignatureTransfer.PermitTransferFrom memory permit,
+        uint256 privateKey,
+        bytes32 domainSeparator
+    ) internal view returns (bytes memory sig) {
+        bytes32 tokenPermissions = keccak256(abi.encode(_TOKEN_PERMISSIONS_TYPEHASH, permit.permitted));
+        bytes32 msgHash = keccak256(
+            abi.encodePacked(
+                "\x19\x01",
+                domainSeparator,
+                keccak256(
+                    abi.encode(
+                        _PERMIT_TRANSFER_FROM_TYPEHASH, tokenPermissions, address(vault), permit.nonce, permit.deadline
+                    )
+                )
+            )
+        );
+
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(privateKey, msgHash);
+        return bytes.concat(r, s, bytes1(v));
+    }
+}
